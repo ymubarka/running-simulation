3b3a454ced35656a15540a8e912849f6bc1066c9
Mubarka, Yamen <mubarka1@ml-9675788.the-lab.llnl.gov>
2020-02-20 13:50:18 -0800

    Added prediction plots

--- scripts/learn.py
+++ scripts/learn.py
@@ -11,7 +11,7 @@ parser.add_argument('-specroot', help='The DAG spec root')
 
 args = parser.parse_args()
 
-training_percent = 0.5
+training_percent = 0.6
 timestep = -1
 
 SPECROOT = args.specroot
@@ -46,3 +46,48 @@ print('testing score: ', regr.score(X_test, y_test))
 print(mean_squared_error(y_test, regr.predict(X_test)))
 
 dump(regr, 'trained-model.joblib')
+
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(1, 2, figsize=(25, 12))
+plt.rcParams.update({'font.size': 12})
+plt.rcParams['lines.linewidth'] = 5
+
+ax[0].scatter(y_train[:,0], y_train[:,1], label='Actual')
+y_pred = regr.predict(X_train)
+ax[0].scatter(y_pred[:,0], y_pred[:,1], label='Predicted')
+ax[0].set_title('Velocity Magnitude %s'%timestep)
+
+ax[0].set_xlabel('Log10 Lidspeed')
+ax[0].set_ylabel('Log10 Viscocity')
+ax[0].set_title('Training Data, # Points: %s'%len(y_pred))
+ax[0].legend()
+ax[0].grid()
+
+x_min = np.min([np.min(y_train[:,0]), np.min(y_pred[:,0])])
+y_min = np.min([np.min(y_train[:,1]), np.min(y_pred[:,1])])
+x_max = np.max([np.max(y_train[:,0]), np.max(y_pred[:,0])])
+y_max = np.max([np.max(y_train[:,1]), np.max(y_pred[:,1])])
+
+ax[1].scatter(y_test[:,0], y_test[:,1], label='Actual')
+
+y_pred = regr.predict(X_test)
+ax[1].scatter(y_pred[:,0], y_pred[:,1], label='Predicted')
+
+ax[1].set_xlabel('Log10 Lidspeed')
+ax[1].set_ylabel('Log10 Viscocity')
+ax[1].set_title('Testing Data, # Points: %s'%len(y_pred))
+ax[1].legend()
+ax[1].grid()
+
+x_min = np.min([np.min(y_test[:,0]), np.min(y_pred[:,0]), x_min]) - 0.1
+y_min = np.min([np.min(y_test[:,1]), np.min(y_pred[:,1]), y_min]) - 0.1
+x_max = np.max([np.max(y_test[:,0]), np.max(y_pred[:,0]), x_max]) + 0.1
+y_max = np.max([np.max(y_test[:,1]), np.max(y_pred[:,1]), y_max]) + 0.1
+
+ax[0].set_xlim([x_min, x_max])
+ax[0].set_ylim([y_min, y_max])
+ax[1].set_xlim([x_min, x_max])
+ax[1].set_ylim([y_min, y_max])
+
+plt.savefig('prediction.png')

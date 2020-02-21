31eff3f85ab6c83131d97d09bccffa5c9b013b5f
Mubarka, Yamen <mubarka1@ml-9675788.the-lab.llnl.gov>
2020-02-21 09:46:34 -0800

    cosmetic changes

--- scripts/cavity-setup.sh
+++ scripts/cavity-setup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+MERLIN_PATH=$1
+SPECROOT=$2
+
 docker create -ti --name temp-container cfdengine/openfoam bash
 docker cp temp-container:/opt/openfoam6/tutorials/incompressible/icoFoam/cavity/cavity .
 docker rm -f temp-container
@@ -5,7 +10,7 @@ docker rm -f temp-container
 cd cavity
 
 echo "***** Setting Up Mesh *****"
-$1/python $2/scripts/mesh-param-script.py -scripts_dir $2/scripts/
+$MERLIN_PATH/python $SPECROOT/scripts/mesh-param-script.py -scripts_dir $SPECROOT/scripts/
 mv blockMeshDict.txt system/blockMeshDict
 sed -i '' "30s/.*/writeControl    runTime;/" system/controlDict
 sed -i '' "26s/.*/endTime         10;/" system/controlDict

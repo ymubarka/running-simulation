bc69443ddf3b5a0816a8778ba18617eaeb4cd0c0
Mubarka, Yamen <mubarka1@llnl.gov>
2020-02-27 10:04:44 -0800

    simplified and standerized the dag

--- scripts/cavity-setup.sh
+++ scripts/cavity-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SPECROOT=$1
+MERLIN_INFO=$1
 
 docker create -ti --name temp-container cfdengine/openfoam bash
-docker cp temp-container:/opt/openfoam6/tutorials/incompressible/icoFoam/cavity/cavity .
+docker cp temp-container:/opt/openfoam6/tutorials/incompressible/icoFoam/cavity/cavity $MERLIN_INFO/
 docker rm -f temp-container
 
-cd cavity
+cd $MERLIN_INFO/cavity
 
 echo "***** Setting Up Mesh *****"
-python $SPECROOT/scripts/mesh-param-script.py -scripts_dir $SPECROOT/scripts/
+python $MERLIN_INFO/scripts/mesh-param-script.py -scripts_dir $MERLIN_INFO/scripts/
 mv blockMeshDict.txt system/blockMeshDict
 sed -i '' "30s/.*/writeControl    runTime;/" system/controlDict
-sed -i '' "26s/.*/endTime         10;/" system/controlDict
-sed -i '' "32s/.*/writeInterval   1;/" system/controlDict
+sed -i '' "26s/.*/endTime         1;/" system/controlDict
+sed -i '' "32s/.*/writeInterval   .1;/" system/controlDict

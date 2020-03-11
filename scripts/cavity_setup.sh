63a6733522dff41f2f0bb24c0739436271594fd3
Mubarka, Yamen <mubarka1@llnl.gov>
2020-03-10 18:54:44 -0700

    merged with the examples version

--- scripts/cavity_setup.sh
+++ scripts/cavity_setup.sh
@@ -9,8 +9,10 @@ docker rm -f temp-container
 cd $MERLIN_INFO/cavity
 
 echo "***** Setting Up Mesh *****"
-python $MERLIN_INFO/scripts/mesh-param-script.py -scripts_dir $MERLIN_INFO/scripts/
+python $MERLIN_INFO/scripts/mesh_param_script.py -scripts_dir $MERLIN_INFO/scripts/
 mv blockMeshDict.txt system/blockMeshDict
+
+echo "***** Setting Control Dictionary *****"
 sed -i '' "30s/.*/writeControl    runTime;/" system/controlDict
 sed -i '' "26s/.*/endTime         1;/" system/controlDict
 sed -i '' "32s/.*/writeInterval   .1;/" system/controlDict
